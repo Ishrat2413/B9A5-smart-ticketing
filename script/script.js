1f745cb6bf38f7e69b08b352c48a4f19ce79290b
Ishrat2413 <ishratrintu2413@gmail.com>
2024-02-18 19:16:02 +0600

    seat increment, decrement, not same or more than 4 seats problem solved

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
-// alert("okay");
+const buttonParent = document.getElementById("seat-buttons");
+const allButtons = buttonParent.getElementsByClassName("btn");
+let count1 = 0;
+const perSeat = document.getElementById("per-seat");
+let count2 = parseInt(perSeat.innerText);
+
+let buttonInner = [];
+for (const btn of allButtons) {
+  btn.addEventListener("click", function (e) {
+    if (buttonInner.includes(btn.innerText)) {
+      alert("You can not book same seat again");
+    } else if (count1 >= 4) {
+      alert("You can not book more than 4 seats");
+    } else {
+      count1 = count1 + 1;
+      count2 = count2 - 1;
+
+      buttonInner.push(btn.innerText);
+      console.log(buttonInner);
+      setInnerText("seat-booked", count1);
+      setInnerText("per-seat", count2);
+      setBackgroundColor(btn);
+    }
+  });
+}
---
+++ script/uti.js
@@ -0,0 +1,6 @@
+function setInnerText(elementId, value) {
+  document.getElementById(elementId).innerText = value;
+}
+function setBackgroundColor(elementId) {
+  const bgColor = elementId.classList.add("bg-[#1DD100]");
+}
